a8a1650accdf1457ee4bd327a217ed1e94294266
MarcinMoskala <marcinmoskala@gmail.com>
2017-02-02 12:41:22 +0100

    Fix

--- sample/kotlinapp/src/main/java/com/marcinmoskala/kotlinapp/SecondActivity.kt
+++ sample/kotlinapp/src/main/java/com/marcinmoskala/kotlinapp/SecondActivity.kt
@@ -13,5 +13,6 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
     }
 }
