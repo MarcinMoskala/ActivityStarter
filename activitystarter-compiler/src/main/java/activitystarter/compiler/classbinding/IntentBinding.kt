98bb828655250cdaf9b0208869345e1d041304cd
MarcinMoskala <marcinmoskala@gmail.com>
2017-02-10 14:37:03 +0100

    Code clean-up

--- activitystarter-compiler/src/main/java/activitystarter/compiler/classbinding/IntentBinding.kt
+++ activitystarter-compiler/src/main/java/activitystarter/compiler/classbinding/IntentBinding.kt
@@ -20,57 +20,53 @@ internal abstract class IntentBinding(element: TypeElement) : ClassBinding(eleme
                     .build()!!
 
     protected fun createGetIntentMethod(variant: List<ArgumentBinding>): MethodSpec {
-        val builder = MethodSpec.methodBuilder("getIntent")
-                .addParameter(CONTEXT, "context")
+        val builder = builderWithCreationBasicFields("getIntent")
+                .addArgParameters(variant)
                 .returns(INTENT)
-                .addModifiers(PUBLIC, STATIC)
 
-        variant.forEach { arg -> builder.addParameter(arg.type, arg.name) }
         builder.addStatement("\$T intent = new Intent(context, \$T.class)", INTENT, targetTypeName)
         variant.forEach { arg -> builder.addStatement("intent.putExtra(\"" + getKey(arg.name) + "\", " + arg.name + ")") }
         builder.addStatement("return intent")
         return builder.build()
     }
 
-    protected fun MethodSpec.Builder.addSetters(targetParameterName: String): MethodSpec.Builder {
+    protected fun MethodSpec.Builder.addSetters(targetParameterName: String) = apply {
         for (arg in argumentBindings) {
-            val fieldName = arg.name
-            val keyName = getKey(fieldName)
+            val keyName = getKey(arg.name)
             val settingPart = arg.accessor.setToField(getIntentGetterFor(arg, keyName))
             addStatement("if(intent.hasExtra(\"$keyName\")) $targetParameterName.$settingPart")
         }
-        return this
     }
 
-    protected fun createGetIntentStarter(starterFunc: String, variant: List<ArgumentBinding>): MethodSpec {
-        val builder = MethodSpec.methodBuilder("start")
-                .addParameter(CONTEXT, "context")
-                .addModifiers(PUBLIC, STATIC)
+    protected fun createGetIntentStarter(starterFunc: String, variant: List<ArgumentBinding>)
+            = builderWithCreationBasicFields("start")
+            .addArgParameters(variant)
+            .addGetIntentStatement(variant)
+            .addStatement("context.$starterFunc(intent)")
+            .build()
 
-        variant.forEach { builder.addParameter(it.type, it.name) }
-        addGetIntentStatement(builder, variant)
-        builder.addStatement("context.$starterFunc(intent)")
-        return builder.build()
+    protected fun builderWitGetIntentWithFlags(variant: List<ArgumentBinding>)
+            = builderWithCreationBasicFields("startWithFlags")
+            .addArgParameters(variant)
+            .addParameter(TypeName.INT, "flags")
+            .addGetIntentStatement(variant)
+            .addStatement("intent.addFlags(flags)")
+
+    protected fun builderWithCreationBasicFields(name: String)
+            = MethodSpec.methodBuilder(name)
+            .addParameter(CONTEXT, "context")
+            .addModifiers(PUBLIC, STATIC)
+
+    private fun MethodSpec.Builder.addArgParameters(variant: List<ArgumentBinding>) = apply {
+        variant.forEach { arg -> addParameter(arg.type, arg.name) }
     }
 
-    protected fun builderWitGetIntentWithFlags(variant: List<ArgumentBinding>): MethodSpec.Builder {
-        val builder = MethodSpec.methodBuilder("startWithFlags")
-                .addParameter(CONTEXT, "context")
-                .addModifiers(PUBLIC, STATIC)
-
-        variant.forEach { builder.addParameter(it.type, it.name) }
-        builder.addParameter(TypeName.INT, "flags")
-        addGetIntentStatement(builder, variant)
-        builder.addStatement("intent.addFlags(flags)")
-        return builder
-    }
-
-    protected fun addGetIntentStatement(builder: MethodSpec.Builder, variant: List<ArgumentBinding>) {
+    protected fun MethodSpec.Builder.addGetIntentStatement(variant: List<ArgumentBinding>) = apply {
         if (variant.isEmpty())
-            builder.addStatement("\$T intent = getIntent(context)", INTENT)
+            addStatement("\$T intent = getIntent(context)", INTENT)
         else {
             val intentArguments = variant.joinToString(separator = ", ", transform = { it.name })
-            builder.addStatement("\$T intent = getIntent(context, $intentArguments)", INTENT)
+            addStatement("\$T intent = getIntent(context, $intentArguments)", INTENT)
         }
     }
 
@@ -81,12 +77,12 @@ internal abstract class IntentBinding(element: TypeElement) : ClassBinding(eleme
         TypeName.BOOLEAN -> "intent.getBooleanExtra(\"$keyName\", false)"
         TypeName.DOUBLE -> "intent.getDoubleExtra(\"$keyName\", -1D)"
         TypeName.CHAR -> "intent.getCharExtra(\"$keyName\", 'a')"
-        else -> when {
-            arg.elementType.isSubtypeOfType("android.os.Parcelable") ->
-                "(${arg.type}) intent.getParcelableExtra(\"$keyName\")"
-            arg.elementType.isSubtypeOfType("java.io.Serializable") ->
-                "(${arg.type}) intent.getSerializableExtra(\"$keyName\")"
-            else -> throw Error("Illegal field type" + arg.type)
-        }
+        else -> getIntentGetterForNotTrival(arg, keyName)
+    }
+
+    private fun getIntentGetterForNotTrival(arg: ArgumentBinding, keyName: String) = when {
+        arg.elementType.isSubtypeOfType("android.os.Parcelable") -> "(${arg.type}) intent.getParcelableExtra(\"$keyName\")"
+        arg.elementType.isSubtypeOfType("java.io.Serializable") -> "(${arg.type}) intent.getSerializableExtra(\"$keyName\")"
+        else -> throw Error("Illegal field type" + arg.type)
     }
 }
