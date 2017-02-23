fdb7e029be111c519706ea92fedcbab71dd8a779
MarcinMoskala <marcinmoskala@gmail.com>
2017-02-23 14:35:18 +0100

    Fix

--- activitystarter-compiler/src/main/java/activitystarter/compiler/ArgumentBinding.kt
+++ activitystarter-compiler/src/main/java/activitystarter/compiler/ArgumentBinding.kt
@@ -8,9 +8,15 @@ import javax.lang.model.type.TypeMirror
 
 class ArgumentBinding(element: Element) {
     val name: String = element.simpleName.toString()
-    val key: String = element.getAnnotation(Arg::class.java)?.key ?: (name + "StarterKey")
+    val key: String
     val elementType: TypeMirror = getElementType(element)
     val type: TypeName = TypeName.get(elementType)
     val isOptional: Boolean = element.getAnnotation(Optional::class.java) != null
     val accessor: FieldAccessor = FieldAccessor(element)
+
+    init {
+        val keyFromAnnotation = element.getAnnotation(Arg::class.java)?.key
+        val defaultKey = name + "StarterKey"
+        key = if(keyFromAnnotation.isNullOrBlank()) defaultKey else keyFromAnnotation!!
+    }
 }
