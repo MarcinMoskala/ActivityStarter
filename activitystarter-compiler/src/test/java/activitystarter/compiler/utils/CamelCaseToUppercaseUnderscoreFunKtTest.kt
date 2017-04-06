0cf78b4f689a74523c9152d8e3dfbe8062fe44d8
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-06 15:34:49 +0200

    Simple upperscore camelcase translations to uppercase underscore.

--- activitystarter-compiler/src/test/java/activitystarter/compiler/utils/CamelCaseToUppercaseUnderscoreFunKtTest.kt
+++ activitystarter-compiler/src/test/java/activitystarter/compiler/utils/CamelCaseToUppercaseUnderscoreFunKtTest.kt
@@ -15,4 +15,10 @@ class CamelCaseToUppercaseUnderscoreFunKtTest {
         assertEquals("KO_KO_KO", camelCaseToUppercaseUnderscore("koKoKo"))
     }
 
+
+    @Test fun `Simple upperscore camelcase translations to uppercase underscore`() {
+        assertEquals("JA_MAN", camelCaseToUppercaseUnderscore("JaMan"))
+        assertEquals("KO_KO_KO", camelCaseToUppercaseUnderscore("KoKoKo"))
+    }
+
 }
