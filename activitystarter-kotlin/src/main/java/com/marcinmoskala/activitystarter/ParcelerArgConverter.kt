0befc04d9e414b73159be94a064888c8c73920df
MarcinMoskala <marcinmoskala@gmail.com>
2017-07-25 00:57:28 +0200

    Kotlin support for Activity

--- activitystarter-kotlin/src/main/java/com/marcinmoskala/activitystarter/ParcelerArgConverter.kt
+++ activitystarter-kotlin/src/main/java/com/marcinmoskala/activitystarter/ParcelerArgConverter.kt
@@ -6,23 +6,24 @@ import android.os.Parcelable
 import java.io.Serializable
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
-import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
+import kotlin.reflect.KType
+import kotlin.reflect.jvm.javaType
 
-inline fun <reified T> argExtra(default: T? = null) = ArgExtraDelegateFactory(T::class, default)
+inline fun <reified T> argExtra(default: T? = null) = ArgExtraDelegateFactory(T::class.java, default)
 
 /* Currently not supported features:
  * - Custom keys
  */
-class ArgExtraDelegateFactory<T>(val clazz: KClass<*>, val default: T?) {
+class ArgExtraDelegateFactory<T>(val clazz: Class<T>, val default: T?) {
 
-//    operator fun provideDelegate(thisRef: Activity, prop: KProperty<*>): ReadOnlyProperty<Activity, T> {
-//        val key = getKey(thisRef::class.java, prop)
-//        val getter = makeIntentGetter(key, default)
-//    }
+    operator fun provideDelegate(thisRef: Activity, prop: KProperty<*>): Lazy<T> {
+        val key = getKey(thisRef::class.java, prop)
+        val getter = makeIntentGetter(key, prop.returnType, default)
+        return lazy { (getter(thisRef.intent) ?: default) as T }
+    }
 
-    private fun makeIntentGetter(key: String, default: T?): (Intent) -> Any? = when (clazz.java.canonicalName) {
+    private fun makeIntentGetter(key: String, type: KType, default: T?): (Intent) -> Any? = when (clazz.canonicalName) {
         "java.lang.String" -> { intent: Intent -> intent.getStringExtra(key) }
         "java.lang.Integer" -> { intent: Intent -> intent.getIntExtra(key, default as? Int ?: -1) }
         "java.lang.Long" -> { intent: Intent -> intent.getLongExtra(key, default as? Long ?: -1) }
@@ -45,60 +46,21 @@ class ArgExtraDelegateFactory<T>(val clazz: KClass<*>, val default: T?) {
         "short[]" -> { intent: Intent -> intent.getShortArrayExtra(key) }
         "java.lang.CharSequence[]" -> { intent: Intent -> intent.getCharSequenceArrayExtra(key) }
 
-        "java.util.ArrayList<java.lang.Integer>" -> { intent: Intent -> intent.getIntegerArrayListExtra(key) }
-        "java.util.ArrayList<java.lang.String>" -> { intent: Intent -> intent.getStringArrayListExtra(key) }
-        "java.util.ArrayList<java.lang.CharSequence>" -> { intent: Intent -> intent.getCharSequenceArrayListExtra(key) }
+        "java.util.ArrayList" -> when (type.javaType.toString()) {
+            "java.util.ArrayList<java.lang.Integer>" -> { intent: Intent -> intent.getIntegerArrayListExtra(key) }
+            "java.util.ArrayList<java.lang.String>" -> { intent: Intent -> intent.getStringArrayListExtra(key) }
+            "java.util.ArrayList<java.lang.CharSequence>" -> { intent: Intent -> intent.getCharSequenceArrayListExtra(key) }
+            else -> (throw TypeNotSupportedError()) as (Intent) -> Any?
+        }
 
-        else -> getSubtypes(key)
+        else -> when {
+            Parcelable::class.java.isAssignableFrom(clazz) -> { intent: Intent -> intent.getParcelableExtra<Parcelable>(key) }
+            Serializable::class.java.isAssignableFrom(clazz) -> { intent: Intent -> intent.getSerializableExtra(key) }
+            else -> (throw TypeNotSupportedError()) as (Intent) -> Any?
+        }
     }
-
-    private fun getSubtypes(key: String): (Intent) -> Any? = when {
-        Parcelable::class.java.isAssignableFrom(clazz.java) -> { intent: Intent -> intent.getParcelableExtra<Parcelable>(key) }
-        Serializable::class.java.isAssignableFrom(clazz.java) -> { intent: Intent -> intent.getSerializableExtra(key) }
-        else -> throw TypeNotSupportedError()
-    }
-
 }
 
 private fun getKey(contextRef: Class<*>, property: KProperty<*>) = "${contextRef.canonicalName}.${property.name}StarterKey"
 
 class TypeNotSupportedError : Error("Type not supported")
-
-// Lack of getTypeName in Type for current Java. Should be changed in the future
-inline fun <reified T: Any> printType() {
-    val type = when {
-        T::class.javaPrimitiveType != null -> T::class.qualifiedName
-        else -> object : TypeReference<T>() {}.type.toString()
-    }
-    println(type)
-}
-
-abstract class TypeReference<T> {
-    val type: Type = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
-}
-
-fun main(args: Array<String>) {
-    printType<String>()
-    printType<Int>()
-    printType<Long>()
-    printType<Float>()
-    printType<Boolean>()
-    printType<Double>()
-    printType<Char>()
-    printType<Byte>()
-    printType<Short>()
-    printType<CharSequence>()
-    printType<BooleanArray>()
-    printType<ByteArray>()
-    printType<ShortArray>()
-    printType<CharArray>()
-    printType<IntArray>()
-    printType<LongArray>()
-    printType<FloatArray>()
-    printType<DoubleArray>()
-    printType<Array<String>>()
-    printType<Array<CharSequence>>()
-    printType<ArrayList<Int>>()
-    printType<ArrayList<String>>()
-    printType<ArrayList<CharSequence>>()
-}
