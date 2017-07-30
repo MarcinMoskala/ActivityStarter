691384570403cc5e9444ba23b966d9df1f248f8b
MarcinMoskala <marcinmoskala@gmail.com>
2017-07-30 22:32:40 +0200

    Improve delegation

--- activitystarter-kotlin/src/main/java/com/marcinmoskala/activitystarter/ArgExtra.kt
+++ activitystarter-kotlin/src/main/java/com/marcinmoskala/activitystarter/ArgExtra.kt
@@ -7,36 +7,52 @@ import java.lang.reflect.Method
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-fun <T> Activity.argExtra(default: T? = null) = ArgExtraDelegateFactory(default)
+fun <T> Activity.argExtra(default: T? = null): ReadWriteProperty<Any, T> = BoundToValueDelegate(default)
 
-fun <T> Fragment.argExtra(default: T? = null) = ArgExtraDelegateFactory(default)
+fun <T> Fragment.argExtra(default: T? = null): ReadWriteProperty<Any, T> = BoundToValueDelegate(default)
 
-fun <T> android.support.v4.app.Fragment.argExtra(default: T? = null) = ArgExtraDelegateFactory(default)
+fun <T> android.support.v4.app.Fragment.argExtra(default: T? = null): ReadWriteProperty<Any, T> = BoundToValueDelegate(default)
 
-class ArgExtraDelegateFactory<T>(val default: T?) {
 
-    operator fun provideDelegate(thisRef: Activity, prop: KProperty<*>): ReadWriteProperty<Any, T>
-            = createDelegate(thisRef, thisRef.javaClass, prop.name)
+private class BoundToValueDelegate<T>(var default: T?) : ReadWriteProperty<Any, T> {
 
-    operator fun provideDelegate(thisRef: Fragment, prop: KProperty<*>): ReadWriteProperty<Any, T>
-            = createDelegate(thisRef, thisRef.javaClass, prop.name)
+    var valueSet = false
+    var value: T? = null
 
-    operator fun provideDelegate(thisRef: android.support.v4.app.Fragment, prop: KProperty<*>): ReadWriteProperty<Any, T>
-            = createDelegate(thisRef, thisRef.javaClass, prop.name)
-
-    private fun createDelegate(thisRef: Any, javaClass: Class<*>, fieldName: String): ReadWriteProperty<Any, T> {
-        val starterClass = getStarterClass(javaClass)
-        val checkerName = ActivityStarterNameConstruction.getterFieldCheckerName(fieldName)
-        val accessorName = ActivityStarterNameConstruction.getterFieldAccessorName(fieldName)
-        val checkerMethod = getMethod(starterClass, checkerName, javaClass)
-        val accessorMethod = getMethod(starterClass, accessorName, javaClass)
-        return BoundToValueDelegate {
-            val argFilled = checkerMethod.invokeMethod(thisRef) as Boolean
-            val argValue: Any? = if (argFilled) accessorMethod.invokeMethod(thisRef) else null
-            val argValueOrDefault = argValue ?: default
-            argValueOrDefault as T
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        synchronized(this) {
+            if (valueSet) @Suppress("UNCHECKED_CAST") return value as T
+            val typedValue = getValueFromStarter(thisRef, thisRef.javaClass, property.name, default)
+            setNewValue(typedValue)
+            @Suppress("UNCHECKED_CAST") return typedValue as T
         }
     }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, newValue: T) {
+        synchronized(this) {
+            setNewValue(newValue)
+        }
+    }
+
+    private fun setNewValue(typedValue: T?) {
+        value = typedValue
+        valueSet = true
+        default = null
+    }
+
+    override fun toString(): String = if (valueSet) value.toString() else "Lazy value not initialized yet."
+}
+
+private fun <T> getValueFromStarter(thisRef: Any, javaClass: Class<*>, fieldName: String, default: T?): T {
+    val starterClass = getStarterClass(javaClass)
+    val checkerName = ActivityStarterNameConstruction.getterFieldCheckerName(fieldName)
+    val accessorName = ActivityStarterNameConstruction.getterFieldAccessorName(fieldName)
+    val checkerMethod = getMethod(starterClass, checkerName, javaClass)
+    val accessorMethod = getMethod(starterClass, accessorName, javaClass)
+    val argFilled = checkerMethod.invokeMethod(thisRef) as Boolean
+    val argValue: Any? = if (argFilled) accessorMethod.invokeMethod(thisRef) else null
+    val argValueOrDefault = argValue ?: default
+    return argValueOrDefault as T
 }
 
 private fun getStarterClass(targetClass: Class<*>): Class<*> {
@@ -59,33 +75,4 @@ private fun Method.invokeMethod(vararg args: Any) = try {
     throw RuntimeException("Unable to invoke " + this + "because of illegal access", e)
 } catch (e: IllegalArgumentException) {
     throw RuntimeException("Unable to invoke $this because of illegal argument", e)
-}
-
-private class BoundToValueDelegate<T>(var initializer: (() -> T)?) : ReadWriteProperty<Any?, T> {
-
-    var valueSet = false
-    var value: T? = null
-
-    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        synchronized(this) {
-            if (valueSet) @Suppress("UNCHECKED_CAST") return value as T
-            val typedValue = initializer?.invoke()
-            setNewValue(typedValue)
-            @Suppress("UNCHECKED_CAST") return typedValue as T
-        }
-    }
-
-    override fun setValue(thisRef: Any?, property: KProperty<*>, newValue: T) {
-        synchronized(this) {
-            setNewValue(newValue)
-        }
-    }
-
-    private fun setNewValue(typedValue: T?) {
-        value = typedValue
-        valueSet = true
-        initializer = null
-    }
-
-    override fun toString(): String = if (valueSet) value.toString() else "Lazy value not initialized yet."
 }
